1e0e72e9a86fea14ba6bf861362c641c92691475
GuillaumeCa <guillaume.carre@isep.fr>
2018-01-04 23:19:25 +0100

    improve example

--- examples/wordcount/master.js
+++ examples/wordcount/master.js
@@ -1,41 +1,55 @@
 const mapreduce = require('../../index')
 const fs = require('fs')
+const path = require('path')
 
 const master = mapreduce.Master({
   port: 8080,
-})
-
-
-const job = master.processFile({
-  url: './files/test.txt',
-  separator: '\n',
 })
 
 if (!fs.existsSync('./out')) {
   fs.mkdirSync('./out')
 }
 
-job
-  .map('wordCount', 3)
-  .reduce('wordCount', 2)
-  .result((data, duration) => {
-    // écrit les résultats dans un fichier
-    let lines = data.map(kv => `${kv.key}: ${kv.value}`).join('\n')
-    const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60))
-    const seconds = Math.floor((duration % (1000 * 60 * 60)) / 1000)
-    const ms = Math.floor(duration % (1000 * 60 * 60))
-    lines = `time: ${minutes}m${seconds}s${ms}ms\n${lines}`
-    console.log('writing results..')
-    fs.writeFile('./out/out.txt', lines, (err) => {
-      if (err) {
-        console.error(err)
-      }
-      console.log('success !')
-    })
+function processFile(file) {
+  const job = master.processFile({
+    url: `./files/${file}`,
+    separator: '\n',
   })
 
+  job
+    .map('wordCount', 3)
+    .reduce('wordCount', 2)
+    .result((data, duration) => {
+      // écrit les résultats dans un fichier
+      let lines = data.map(kv => `${kv.key}: ${kv.value}`).join('\n')
+      const minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60))
+      const seconds = Math.floor((duration % (1000 * 60 * 60)) / 1000)
+      const ms = Math.floor(duration % (1000 * 60 * 60))
+      lines = `time: ${minutes}m${seconds}s${ms}ms\n${lines}`
+      console.log('writing results..')
+      fs.writeFile(`./out/out-${path.parse(file).name}.txt`, lines, (err) => {
+        if (err) {
+          console.error(err)
+        }
+        console.log('success !')
+      })
+    })
 
-// setTimeout(() => {
-console.log('running job..')
-job.run()
-// }, 5000);
+  console.log('running job..')
+  job.run()
+}
+
+function run() {
+  fs.readdir('./files', (err, fileNames) => {
+    if (err) throw err
+    fileNames.forEach(fileName => {
+      // if not a hidden file
+      if (fileName.charAt(0) !== '.') {
+        processFile(fileName)
+      }
+    })
+  })
+}
+
+run()
+setInterval(run, 10 * 1000)
